f63f47cd15971aec95e31eca720495d32320b7e4
tahhh <thadpongsermsook1@gmail.com>
2021-12-15 13:57:54 +0700

    Lab11_3 completed

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,27 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int count = 0;
+    double sum = 0, mean, stdDev, lol = 0;
+    ifstream source;
+    source.open("score.txt");
+    string text;
+    while(getline(source,text)){
+        count++;
+        sum += atof(text.c_str()); 
+        lol += pow(atof(text.c_str()),2);
+    }
+
+    mean = sum/count;
+    stdDev = pow((lol/count)-pow(mean,2),1/2.0);
+
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << stdDev;
+}
+
