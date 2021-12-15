204acb63ee8a112a84d195fb57f7819b9ed21703
tahhh <thadpongsermsook1@gmail.com>
2021-12-15 13:20:09 +0700

    Lab11_2 completed

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -9,8 +9,17 @@ int main (){
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	dest << "-------------------- BOOM ---------------------" << endl;
+	dest << "AR CHI" << endl;
+	dest << "AR BOW" << endl;
+	dest << "CHOW CHOW" << endl;
+	dest << "BA KA" << endl;
+	dest << "BA GEAR" << endl;
+	dest << "CHEER CHEER" << endl;
+	dest << "WE ARE ALL" << endl;
+	dest << "ENTANEER" << endl;
+	dest << "CMU" << endl;
+	dest << "-------------------- HA!! ---------------------";
 
     source.close();
     dest.close();
